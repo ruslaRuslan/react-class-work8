b3b257df8ab87be243cacaa833441cda5b362df3
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-15 15:21:03 +0400

    DataContext.Provider

---
+++ src/contexts/DataContext.jsx
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+
+const DataContext = createContext("")
+
+import React from 'react'
+
+const DataContextProvider = () => {
+  return (
+    <>
+    <DataContext.Provider>
+        
+    </DataContext.Provider>
+    </>
+  )
+}
+
+export default DataContextProvider
